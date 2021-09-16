c31603fa7934c758916e16a00b8e75734299b01f
Tyler Ackerson <tyler.a.ackerson@gmail.com>
2021-09-16 12:45:46 -0700

    PollContract: add getPoll

--- src/smart-contracts/poll.sol
+++ src/smart-contracts/poll.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.7;
 
 contract PollContract {
-
     struct Poll {
         uint256 id;
         string question;
@@ -20,20 +19,41 @@ contract PollContract {
     Poll[] private polls;
     mapping(address => Voter) private voters;
 
-    function createPoll(string memory _question, string memory _image, bytes32[] memory _options) public {
+    function createPoll(
+        string memory _question,
+        string memory _image,
+        bytes32[] memory _options
+    ) public {
         require(bytes(_question).length > 0, "Empty question");
         require(_options.length > 1, "At least 2 options are required");
 
         uint256 pollId = polls.length; // eek...
 
         Poll memory newPoll = Poll({
-           id: pollId,
-           question: _question,
-           image: _image,
-           options: _options,
-           votes: new uint64[](_options.length)
+            id: pollId,
+            question: _question,
+            image: _image,
+            options: _options,
+            votes: new uint64[](_options.length)
         });
 
         polls.push(newPoll);
+    }
+
+    function getPoll(uint256 _pollId)
+        external
+        view
+        returns (
+            uint256,
+            string memory,
+            string memory,
+            uint64[] memory,
+            bytes32[] memory
+        )
+    {
+        require(_pollId < polls.length && _pollId >= 0, "No poll found");
+
+        Poll memory poll = polls[_pollId];
+        return (poll.id, poll.question, poll.image, poll.votes, poll.options);
     }
 }
